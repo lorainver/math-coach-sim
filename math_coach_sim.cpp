2c6e49d32a61c3736ed7990f1e876227c4ff0be3
lorainve <lorainver@qq.com>
2026-06-13 21:12:01 +0800

    refactor: extract question bank to questions.json config file

--- math_coach_sim.cpp
+++ math_coach_sim.cpp
@@ -1,4 +1,4 @@
-﻿#include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 long unsigned int tim = 1000;
 int opt, zhong_lei;
@@ -7,6 +7,180 @@ char op, ca;
 int return_zhi = 0, choose_a = 0, choose_b = 0, hard = 1;
 const int MAX_RATING = 4;
 
+// ===== 题库数据结构 =====
+struct Question {
+	wstring intro;        // 难度提示语
+	wstring question;     // 题目内容
+	string answer_type;   // "int" / "int_pair" / "string_multi"
+	int answer_int;       // answer_type == "int" 时的答案
+	int answer_int_a, answer_int_b; // answer_type == "int_pair" 时的答案
+	vector<string> answer_strings;  // answer_type == "string_multi" 时的所有合法答案
+	int reward;           // 答对奖励
+};
+
+// 按 [category_index][difficulty] 存储, category: 0=代数 1=几何 2=数论 3=组合, difficulty: 0-based
+Question questions[4][3];
+int question_count[4] = {0, 0, 0, 0}; // 每个 category 已加载的题目数
+
+// 读取带引号的 JSON 字符串值（简易解析，不支持转义）
+wstring readJsonStringValue(const wstring &json, size_t &pos) {
+	while (pos < json.size() && json[pos] != L'"') pos++;
+	if (pos >= json.size()) return L"";
+	pos++; // skip opening "
+	wstring result;
+	while (pos < json.size() && json[pos] != L'"') {
+		if (json[pos] == L'\\' && pos + 1 < json.size()) {
+			pos++;
+			if (json[pos] == L'n') result += L'\n';
+			else if (json[pos] == L'"') result += L'"';
+			else if (json[pos] == L'\\') result += L'\\';
+			else result += json[pos];
+		} else {
+			result += json[pos];
+		}
+		pos++;
+	}
+	if (pos < json.size()) pos++; // skip closing "
+	return result;
+}
+
+string wstringToUtf8(const wstring &w) {
+	string result;
+	for (size_t i = 0; i < w.size(); i++) {
+		if (w[i] < 0x80) {
+			result += (char)w[i];
+		} else if (w[i] < 0x800) {
+			result += (char)(0xC0 | (w[i] >> 6));
+			result += (char)(0x80 | (w[i] & 0x3F));
+		} else {
+			result += (char)(0xE0 | (w[i] >> 12));
+			result += (char)(0x80 | ((w[i] >> 6) & 0x3F));
+			result += (char)(0x80 | (w[i] & 0x3F));
+		}
+	}
+	return result;
+}
+
+string readJsonStringUtf8(const wstring &json, size_t &pos) {
+	wstring w = readJsonStringValue(json, pos);
+	return wstringToUtf8(w);
+}
+
+int readJsonInt(const wstring &json, size_t &pos) {
+	while (pos < json.size() && (json[pos] == L' ' || json[pos] == L':' || json[pos] == L',')) pos++;
+	int sign = 1;
+	if (pos < json.size() && json[pos] == L'-') { sign = -1; pos++; }
+	int val = 0;
+	while (pos < json.size() && json[pos] >= L'0' && json[pos] <= L'9') {
+		val = val * 10 + (json[pos] - L'0');
+		pos++;
+	}
+	return sign * val;
+}
+
+// 加载题库
+void loadQuestions(const string &filename) {
+	// 读取文件为 wstring
+	wifstream fin(filename);
+	if (!fin.is_open()) {
+		wchar_t err[] = L"错误：无法打开题库文件 questions.json";
+		wprintf(L"%ls", err);
+		cout << "\n";
+		return;
+	}
+	fin.imbue(locale(""));
+	wstring json((istreambuf_iterator<wchar_t>(fin)), istreambuf_iterator<wchar_t>());
+	fin.close();
+
+	size_t pos = 0;
+	while (pos < json.size()) {
+		// 找到每个题目对象的开始 {
+		while (pos < json.size() && json[pos] != L'{') pos++;
+		if (pos >= json.size()) break;
+		pos++; // skip {
+
+		wstring category;
+		int difficulty = 0;
+		Question q;
+
+		// 解析字段
+		while (pos < json.size()) {
+			// 跳过空白和逗号
+			while (pos < json.size() && (json[pos] == L' ' || json[pos] == L'\n' || json[pos] == L'\r' || json[pos] == L'\t' || json[pos] == L',')) pos++;
+			if (pos >= json.size() || json[pos] == L'}') break;
+
+			// 读 key
+			wstring key = readJsonStringValue(json, pos);
+
+			// 跳过冒号
+			while (pos < json.size() && json[pos] != L':') pos++;
+			pos++;
+
+			if (key == L"category") {
+				category = readJsonStringValue(json, pos);
+			} else if (key == L"difficulty") {
+				difficulty = readJsonInt(json, pos);
+			} else if (key == L"intro") {
+				q.intro = readJsonStringValue(json, pos);
+			} else if (key == L"question") {
+				q.question = readJsonStringValue(json, pos);
+			} else if (key == L"answer_type") {
+				q.answer_type = readJsonStringUtf8(json, pos);
+			} else if (key == L"answer") {
+				// 跳过空白
+				while (pos < json.size() && (json[pos] == L' ' || json[pos] == L'\n' || json[pos] == L'\r' || json[pos] == L'\t')) pos++;
+				if (pos < json.size() && json[pos] == L'[') {
+					// 数组形式: int_pair 或 string_multi
+					pos++; // skip [
+					if (q.answer_type == "int_pair") {
+						q.answer_int_a = readJsonInt(json, pos);
+						q.answer_int_b = readJsonInt(json, pos);
+					} else if (q.answer_type == "string_multi") {
+						while (pos < json.size() && json[pos] != L']') {
+							if (json[pos] == L'"') {
+								string s = readJsonStringUtf8(json, pos);
+								q.answer_strings.push_back(s);
+							} else {
+								pos++;
+							}
+						}
+					} else {
+						// single int in array
+						q.answer_int = readJsonInt(json, pos);
+					}
+					while (pos < json.size() && json[pos] != L']') pos++;
+					if (pos < json.size()) pos++; // skip ]
+				} else {
+					// 单个整数
+					q.answer_int = readJsonInt(json, pos);
+				}
+			} else if (key == L"reward") {
+				q.reward = readJsonInt(json, pos);
+			} else {
+				// 跳过未知字段
+				while (pos < json.size() && json[pos] != L',' && json[pos] != L'}') pos++;
+			}
+		}
+
+		// 映射 category 到 index
+		int cat_idx = -1;
+		if (category == L"代数") cat_idx = 0;
+		else if (category == L"几何") cat_idx = 1;
+		else if (category == L"数论") cat_idx = 2;
+		else if (category == L"组合") cat_idx = 3;
+
+		if (cat_idx >= 0 && difficulty >= 1 && difficulty <= 3) {
+			questions[cat_idx][difficulty - 1] = q;
+			question_count[cat_idx]++;
+		}
+
+		// 跳过 }
+		while (pos < json.size() && json[pos] != L'}') pos++;
+		if (pos < json.size()) pos++;
+	}
+}
+
+// ===== 显示函数 =====
 void out_pj(int x) {
 	if (x <= 1) {
 		wchar_t lw1[] = L"差";
@@ -64,266 +238,66 @@ void out(int x) {
 	cout << ".";
 }
 
-int ds() {
-	if (hard == 1) {
+// ===== 做题逻辑（数据驱动） =====
+int do_question(int cat_idx) {
+	int diff_idx = hard - 1; // hard: 1~3, diff_idx: 0~2
+	if (diff_idx < 0 || diff_idx > 2) diff_idx = 0;
+	Question &q = questions[cat_idx][diff_idx];
+
+	wprintf(L"%ls", q.intro.c_str());
+	cout << "\n";
+	_sleep(tim);
+	wprintf(L"%ls", q.question.c_str());
+	cout << "\n";
+
+	if (q.answer_type == "int_pair") {
 		int a, b;
-		wchar_t ww1[] = L"这是一道十分简单的代数题：";
-		wprintf(L"%ls", ww1);
-		cout << "\n";
-		_sleep(tim);
-		wchar_t ww2[] = L"解方程：x^2+3x+2=0 ，请输入其的两根，从小到大";
-		wprintf(L"%ls", ww2);
-		cout << "\n";
 		cin >> a >> b;
-		if (a == -2 && b == -1) {
+		if (a == q.answer_int_a && b == q.answer_int_b) {
 			wchar_t ww3[] = L"恭喜你，答案正确";
 			wprintf(L"%ls", ww3);
 			cout << "\n";
-			return 1;
+			return q.reward;
 		} else {
 			wchar_t ww4[] = L"答案错误！！！";
 			wprintf(L"%ls", ww4);
 			return 0;
 		}
-	} else if (hard == 2) {
+	} else if (q.answer_type == "int") {
 		int a;
-		wchar_t ww1[] = L"这是一道中等难度的代数题：";
-		wprintf(L"%ls", ww1);
-		cout << "\n";
-		_sleep(tim);
-		wchar_t ww2[] = L"(1997全国高联试题）已知实数x,y满足：(x+1)^3+1997(x+1)=-1,(y+1)^3+1997(y+1)=1,则x+y=";
-		wprintf(L"%ls", ww2);
-		cout << "\n";
 		cin >> a;
-		if (a == 2) {
+		if (a == q.answer_int) {
 			wchar_t ww3[] = L"恭喜你，答案正确";
 			wprintf(L"%ls", ww3);
 			cout << "\n";
-			return 1;
+			return q.reward;
 		} else {
 			wchar_t ww4[] = L"答案错误！！！";
 			wprintf(L"%ls", ww4);
 			return 0;
 		}
-	} else {
-		int a;
-		wchar_t ww1[] = L"这是一道比较难的代数题：";
-		wprintf(L"%ls", ww1);
-		cout << "\n";
-		_sleep(tim);
-		wchar_t ww2[] = L"（2025全国高联一试 填空5）f(x)是定义域为R的函数，g(x)=(x-1)f(x)，h(x)=f(x)+x，若g(x)为奇函数，h(x)为偶函数，记f(x)在R上的最大值为m，则4*m=？";
-		wprintf(L"%ls", ww2);
-		cout << "\n";
-		cin >> a;
-		if (a == 1) {
-			wchar_t ww3[] = L"恭喜你，答案正确";
-			wprintf(L"%ls", ww3);
-			cout << "\n";
-			return 2;
-		} else {
-			wchar_t ww4[] = L"答案错误！！！";
-			wprintf(L"%ls", ww4);
-			return 0;
-		}
-	}
-	return 0;
-}
-
-int jh() {
-	if (hard == 1) {
-		int a;
-		wchar_t ww1[] = L"这是一道十分简单的几何题：";
-		wprintf(L"%ls", ww1);
-		cout << "\n";
-		_sleep(tim);
-		wchar_t ww2[] = L"在三角形ABC中，角A=25度，角B=35度，那么角C=多少度？";
-		wprintf(L"%ls", ww2);
-		cout << "\n";
-		cin >> a;
-		if (a == 120) {
-			wchar_t ww3[] = L"恭喜你，答案正确";
-			wprintf(L"%ls", ww3);
-			cout << "\n";
-			return 1;
-		} else {
-			wchar_t ww4[] = L"答案错误！！！";
-			wprintf(L"%ls", ww4);
-			return 0;
-		}
-	} else if (hard == 2) {
-		int a;
-		wchar_t ww1[] = L"这是一道中等难度的几何题：";
-		wprintf(L"%ls", ww1);
-		cout << "\n";
-		_sleep(tim);
-		wchar_t ww2[] = L"在四边形ABCD中，连结对角线AC，BD交于M，角BAC=角BDC=90度，AB=AC=根号5，CD=1，则4*DM=？";
-		wprintf(L"%ls", ww2);
-		cout << "\n";
-		cin >> a;
-		if (a == 2) {
-			wchar_t ww3[] = L"恭喜你，答案正确";
-			wprintf(L"%ls", ww3);
-			cout << "\n";
-			return 1;
-		} else {
-			wchar_t ww4[] = L"答案错误！！！";
-			wprintf(L"%ls", ww4);
-			return 0;
-		}
-	} else {
-		int a;
-		wchar_t ww1[] = L"这是一道非常非非常常非常非常难的几何题：";
-		wprintf(L"%ls", ww1);
-		cout << "\n";
-		_sleep(tim);
-		wchar_t ww2[] = L"（2025全国高联一试试题)正四面体ABCD各棱长为2，，P，Q，分别是棱AB，AC，上的动点（允许位于棱端点），AP+AQ=2，M为棱AD的中点。在三角形MPQ中，MH是PQ的高。设MH的最小值为x,3x^2=?";
-		wprintf(L"%ls", ww2);
-		cout << "\n";
-		cin >> a;
-		if (a == 6) {
-			wchar_t ww3[] = L"恭喜你，答案正确";
-			wprintf(L"%ls", ww3);
-			cout << "\n";
-			return 2;
-		} else {
-			wchar_t ww4[] = L"答案错误！！！";
-			wprintf(L"%ls", ww4);
-			return 0;
-		}
-	}
-	return 0;
-}
-
-int sl() {
-	if (hard == 1) {
-		int a;
-		wchar_t ww1[] = L"这是一道十分简单的数论题：";
-		wprintf(L"%ls", ww1);
-		cout << "\n";
-		_sleep(tim);
-		wchar_t ww2[] = L"多位数123456789101112...202420252026除以9的余数为？";
-		wprintf(L"%ls", ww2);
-		cout << "\n";
-		cin >> a;
-		if (a == 1) {
-			wchar_t ww3[] = L"恭喜你，答案正确";
-			wprintf(L"%ls", ww3);
-			cout << "\n";
-			return 1;
-		} else {
-			wchar_t ww4[] = L"答案错误！！！";
-			wprintf(L"%ls", ww4);
-			return 0;
-		}
-	} else if (hard == 2) {
-		int a;
-		wchar_t ww1[] = L"这是一道中等难度的数论题：";
-		wprintf(L"%ls", ww1);
-		cout << "\n";
-		_sleep(tim);
-		wchar_t ww2[] = L"方程 3^x+4^x=5^x 的正整数解有多少个（无解输出0，无穷解输出-1）？";
-		wprintf(L"%ls", ww2);
-		cout << "\n";
-		cin >> a;
-		if (a == 1) {
-			wchar_t ww3[] = L"恭喜你，答案正确";
-			wprintf(L"%ls", ww3);
-			cout << "\n";
-			return 1;
-		} else {
-			wchar_t ww4[] = L"答案错误！！！";
-			wprintf(L"%ls", ww4);
-			return 0;
-		}
-	} else {
+	} else if (q.answer_type == "string_multi") {
 		string a;
-		wchar_t ww1[] = L"这是一道比较难的数论题（也许吧）：";
-		wprintf(L"%ls", ww1);
-		cout << "\n";
-		_sleep(tim);
-		wchar_t ww2[] = L"因式分解：x^3+4x^2+5x+2 （注：本题设置spj,须按\"()()\"的格式输入，例：(x+1)(x^2+1)(x+1)亦可输入(x+1)(x^2+1)(x+1)）";
-		wprintf(L"%ls", ww2);
-		cout << "\n";
 		cin >> a;
-		if (a == "(x+1)(x+1)(x+2)" || a == "(x+1)(x+2)(x+1)" || a == "(x+2)(x+1)(x+1)") {
-			wchar_t ww3[] = L"恭喜你，答案正确";
-			wprintf(L"%ls", ww3);
-			cout << "\n";
-			return 2;
-		} else {
-			wchar_t ww4[] = L"答案错误！！！";
-			wprintf(L"%ls", ww4);
-			return 0;
+		for (size_t i = 0; i < q.answer_strings.size(); i++) {
+			if (a == q.answer_strings[i]) {
+				wchar_t ww3[] = L"恭喜你，答案正确";
+				wprintf(L"%ls", ww3);
+				cout << "\n";
+				return q.reward;
+			}
 		}
+		wchar_t ww4[] = L"答案错误！！！";
+		wprintf(L"%ls", ww4);
+		return 0;
 	}
+
+	wchar_t ww4[] = L"答案错误！！！";
+	wprintf(L"%ls", ww4);
 	return 0;
 }
 
-int zh() {
-	if (hard == 1) {
-		int a;
-		wchar_t ww1[] = L"这是一道并不十分简单的组合题：";
-		wprintf(L"%ls", ww1);
-		cout << "\n";
-		_sleep(tim);
-		wchar_t ww2[] = L"设r=p/q;0<r<1;p,q为正整数且(p,q)=1;pq|3600;求满足条件的r的个数?";
-		wprintf(L"%ls", ww2);
-		cout << "\n";
-		cin >> a;
-		if (a == 112) {
-			wchar_t ww3[] = L"恭喜你，答案正确";
-			wprintf(L"%ls", ww3);
-			cout << "\n";
-			return 1;
-		} else {
-			wchar_t ww4[] = L"答案错误！！！";
-			wprintf(L"%ls", ww4);
-			return 0;
-		}
-	} else if (hard == 2) {
-		int a;
-		wchar_t ww1[] = L"这是一道中等难度的组合题：";
-		wprintf(L"%ls", ww1);
-		cout << "\n";
-		_sleep(tim);
-		wchar_t ww2[] = L"（2024全国高联一试试题）若三个正整数a,b,c的位数之和为8，且组成a,b,c的8个数码能排列为2，0，2，4，0，9，0，8，则称(a,b,c)为「幸运数组」，例如（9，8，202400）为「幸运数组」，则满足10<a<b<c的幸运数组个数为？";
-		wprintf(L"%ls", ww2);
-		cout << "\n";
-		cin >> a;
-		if (a == 591) {
-			wchar_t ww3[] = L"恭喜你，答案正确";
-			wprintf(L"%ls", ww3);
-			cout << "\n";
-			return 1;
-		} else {
-			wchar_t ww4[] = L"答案错误！！！";
-			wprintf(L"%ls", ww4);
-			return 0;
-		}
-	} else {
-		int a;
-		wchar_t ww1[] = L"这是一道非常非常非常非常非常非常非常非常难的组合题：";
-		wprintf(L"%ls", ww1);
-		cout << "\n";
-		_sleep(tim);
-		wchar_t ww2[] = L"（2025 CMO 试题）设n是正整数，有n张红色卡片与蓝色卡片，最初每张红色卡片上都写有一个实数0，每张蓝色卡片上都写有一个实数1，一次操作是指：选择一张红色卡片与一张蓝色卡片，满足红色卡片上的实数x小于蓝色卡片上的实数y，将这两个实数擦去，并在这两张卡片上都写下实数（x+y)/2,求最小的正整数n，使得可以适当地进行有限次操作，让所有n张红色卡片上的实数之和都大于100？";
-		wprintf(L"%ls", ww2);
-		cout << "\n";
-		cin >> a;
-		if (a == 106) {
-			wchar_t ww3[] = L"恭喜你，答案正确";
-			wprintf(L"%ls", ww3);
-			cout << "\n";
-			return 2;
-		} else {
-			wchar_t ww4[] = L"答案错误！！！";
-			wprintf(L"%ls", ww4);
-			return 0;
-		}
-	}
-	return 0;
-}
-
+// ===== 安全输入 =====
 void safe_cin_int(int &var, const wchar_t *prompt) {
 	while (!(cin >> var)) {
 		cin.clear();
@@ -332,8 +306,13 @@ void safe_cin_int(int &var, const wchar_t *prompt) {
 	}
 }
 
+// ===== 主函数 =====
 int main() {
 	srand(time(0));
+
+	// 加载题库
+	loadQuestions("questions.json");
+
 	cout << "                                              MO";
 	setlocale(LC_ALL, "");
 	wchar_t ws[] = L"教练模拟器";
@@ -439,39 +418,23 @@ int main() {
 		_sleep(tim);
 		system("CLS");
 		hard = (rand() % 3) + 1;
-		if (zhong_lei == 1) {
-			return_zhi = ds();
-			_sleep(tim);
-			system("CLS");
-			dai[choose_a] += return_zhi;
-			dai[choose_b] += return_zhi;
-			clamp_rating(dai[choose_a]);
-			clamp_rating(dai[choose_b]);
-		} else if (zhong_lei == 2) {
-			return_zhi = jh();
-			_sleep(tim);
-			system("CLS");
-			ji[choose_a] += return_zhi;
-			ji[choose_b] += return_zhi;
-			clamp_rating(ji[choose_a]);
-			clamp_rating(ji[choose_b]);
-		} else if (zhong_lei == 3) {
-			return_zhi = sl();
-			_sleep(tim);
-			system("CLS");
-			shu[choose_a] += return_zhi;
-			shu[choose_b] += return_zhi;
-			clamp_rating(shu[choose_a]);
-			clamp_rating(shu[choose_b]);
-		} else {
-			return_zhi = zh();
-			_sleep(tim);
-			system("CLS");
-			zu[choose_a] += return_zhi;
-			zu[choose_b] += return_zhi;
-			clamp_rating(zu[choose_a]);
-			clamp_rating(zu[choose_b]);
-		}
+
+		int cat_idx = zhong_lei - 1; // 0=代数 1=几何 2=数论 3=组合
+		return_zhi = do_question(cat_idx);
+		_sleep(tim);
+		system("CLS");
+
+		int *rating_arr;
+		if (cat_idx == 0) rating_arr = dai;
+		else if (cat_idx == 1) rating_arr = ji;
+		else if (cat_idx == 2) rating_arr = shu;
+		else rating_arr = zu;
+
+		rating_arr[choose_a] += return_zhi;
+		rating_arr[choose_b] += return_zhi;
+		clamp_rating(rating_arr[choose_a]);
+		clamp_rating(rating_arr[choose_b]);
+
 		wchar_t w00[] = L"现在，";
 		wprintf(L"%ls", w00);
 		cout << "\n";
